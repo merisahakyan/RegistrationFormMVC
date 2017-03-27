563f399ee4e430c14c8e7314d1f6155e856c9b0b
merisahakyan <sahakyan_m@bk.ru>
2017-03-27 20:29:07 +0400

    added Validations

---
+++ MyWebPage/MyWebPage/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyWebPage.Models
+{
+    public class User
+    {
+        [Required]
+        [RegularExpression("[a-z A-Z]", ErrorMessage = "Name can contain only letters")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("[a-z A-Z]", ErrorMessage = "Surname can contain only letters")]
+        public string Surname { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string eMail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ MyWebPage/MyWebPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebPage.Startup))]
+namespace MyWebPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
